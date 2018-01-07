5f7efcab546bc16acb56f211ef7994f364a5732e
Fatine Bentires <fatine@macbook-air-de-fatine.home>
2018-01-07 20:35:23 +0100

    core ok quand on regarde réponse score plus incrémenté. Questions et réponses 1 à 20 ok. Reste autre pb de score si on appuie plusieurs fois sur la mbonne reponse le score est incrémenté. Reste pb de pas pouvoir rejouer une autre partie

---
+++ Jour5_soir/Score.cpp
@@ -0,0 +1,53 @@
+#include "Score.hpp"
+
+Score::Score(){_score=0; _essai=1; _a_vu_reponse=false;} //on met le score à 0
+//on prend essai dans la structure de la classe Score comme ça pas de pb de local ou d'effacement 
+
+
+Score::Score(Reponses r){ _r=r;}
+
+void Score::initialisation() //va contenir les chargements d'images des différents scores possibles
+{
+    
+    _images[0]= "/Users/fatine/Desktop/scores/0.png"; //pour le score de zeros
+    _images[1]= "/Users/fatine/Desktop/scores/1.png";
+    _images[2]= "/Users/fatine/Desktop/scores/2.png";
+    _images[3]= "/Users/fatine/Desktop/scores/3.png";
+    _images[4]= "/Users/fatine/Desktop/scores/4.png";
+    _images[5]= "/Users/fatine/Desktop/scores/5.png";
+    _images[6]= "/Users/fatine/Desktop/scores/6.png";
+    _images[7]= "/Users/fatine/Desktop/scores/7.png";
+    _images[8]= "/Users/fatine/Desktop/scores/8.png";
+    _images[9]= "/Users/fatine/Desktop/scores/9.png";
+    _images[10]= "/Users/fatine/Desktop/scores/10.png";
+    
+}
+
+void Score::resultat(int numero_question, int mode, std::string touche) // en fonction de si la personne donne la bonne réponse ou pas
+{
+    // fait appel à la réponse faite par l'utilisateur 
+    std::string reponse_faite=_r.recuperation_reponses(numero_question,mode);
+    bool verdict= get_a_vu_reponse(_a_vu_reponse); //on dit qu'il a vu la reponse
+    std::cout << "reponse_score:" << reponse_faite << std::endl;
+     std::cout << "verdict:" << verdict << std::endl;
+    
+    if(reponse_faite==touche && verdict==false) //si il appuie sur le bon résultat et qu'il n'a pas vu la réponse
+    {
+        _score= _score +1; // si il appuie sur la bonne touche on augmente son score)
+        std::cout << "_score_boucle=" << _score << std::endl;
+        //_essai=false; //le score ne sera augmenté que si elle a raison à la première tentative
+    }
+
+    else
+        _essai=2; // on bloque les possibles erreurs de score
+    
+    std::cout << "i=" << numero_question << "score_orboucle="<<get_score() << std::endl;
+
+    
+}
+
+
+Score::~Score(){}
+
+
+
