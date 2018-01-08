1cb95d70ca9095f25838e5e9e4c106a9ab4d38c8
Fatine Bentires <fatine@canalip380.canalip.upmc.fr>
2018-01-08 17:46:03 +0100

    Reste solidification de detail + rejouer + changer les chemins + mettre les images + makefile ok + finir rapport

---
+++ Jour6_2/Affiche.hpp
@@ -0,0 +1,34 @@
+#ifndef Affiche_hpp
+#define Affiche_hpp
+
+#include <stdio.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include "SDL2_image/SDL_image.h"
+#include <SDL2/SDL_events.h>
+#include <iostream>
+#include "Ecran.hpp"
+
+/*
+ Le but de la classe Affiche qui hérite d'écran est de créer les fonctions qui vont permettre l'affichage à proprement parler. 
+ Ainsi la fonction Init permet d'initialiser, affichage va afficher à l'écran la page d'accueil. 
+ 
+ */
+
+
+class Affiche: public Ecran
+{
+    
+public:
+    Affiche();
+    Affiche(SDL_Window *win,SDL_Renderer *renderer,SDL_Texture *img,int l,int h,const std::string screen_name, std::string PATH);
+    ~Affiche();
+    int Init(Ecran* ecran); // va permettre d'initialiser la première page avec Solo, multi ou contre la montre
+    //void affichage(SDL_Renderer* renderer,SDL_Window* win,SDL_Texture* img,int WIDTH, int HEIGHT,SDL_Rect texr); // affiche
+    void destruction(SDL_Renderer* renderer,SDL_Window* win,SDL_Texture* img);
+    
+};
+
+
+
+#endif /* Affiche_hpp */
