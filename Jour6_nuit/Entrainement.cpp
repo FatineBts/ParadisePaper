0f6d3c398ebc374882e671da2869ecf18f28ba1d
Fatine Bentires <fatine@MacBook-Air-de-Fatine.local>
2018-01-09 01:20:48 +0100

    version ok. Reste modif alex, changement de path + addition des 2 virtuals

---
+++ Jour6_nuit/Entrainement.cpp
@@ -0,0 +1,12 @@
+#include "Entrainement.hpp"
+
+Entrainement::Entrainement(){}
+
+Entrainement::~Entrainement(){}
+
+void Entrainement::initialisation() // va initialiser les pages propres à solo notamment la page d'accueil et les réponses
+{
+    _page.push_back("/Users/fatine/Desktop/images/entrainement.jpg");
+
+}
+
