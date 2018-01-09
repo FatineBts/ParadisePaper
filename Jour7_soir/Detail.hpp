6a56293fc81783341bb90d8e684aef3d273bdb5d
Fatine Bentires <fatine@MacBook-Air-de-Fatine.local>
2018-01-09 18:24:03 +0100

    commentaires ok, UML fait, reste itérations + les images par Alex

---
+++ Jour7_soir/Detail.hpp
@@ -0,0 +1,46 @@
+//
+//  Detail.hpp
+//  Cpp
+//
+//  Created by Fatine Bentires on 08/01/2018.
+//  Copyright © 2018 Fatine Bentires. All rights reserved.
+//
+
+#ifndef Detail_hpp
+#define Detail_hpp
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include "Score.hpp"
+
+/*
+ La classe détail permet de:
+- Une classe initialisation() qui charge les chemins
+- Une fonction détail qui donne les chemins
+- Une fonction verification qui vérifie si la personne à vu la question ou pas 
+ 
+ */
+
+
+
+
+class Detail : public Score
+{
+public:
+    Detail();
+    ~Detail();
+    void initialisation();
+    std::string get_detail(int i){return _detail[i];}
+    bool verification(int i);
+    
+private:
+    std::string _detail[50]; // va contenir les chemins des images détails
+    Score _score;
+    bool _a_vu[50];
+};
+
+
+
+
+#endif /* Detail_hpp */
---
+++ Jour7_soir/Entrainement.hpp
@@ -0,0 +1,31 @@
+#ifndef Solo_hpp
+#define Solo_hpp
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include "SDL2_image/SDL_image.h"
+#include <iostream>
+#include <vector>
+#include "Reponses.hpp"
+
+/*
+ La classe entrainement est composée de:
+    - d'une fonction intialisation(); qui initialise le chargement de l'image et qui enregistre le PATH dans _page
+*/
+
+class Entrainement
+{
+public:
+    Entrainement();
+    ~Entrainement();
+    void initialisation(); // initialise le chemin de l'image pour entrainement 
+    std::vector<std::string> get_page() {return _page;} //pour ne pas modifier
+    std::vector <std::string> _page; // contient le chemin PATH de la page, ne peut pas être modifié
+
+private:
+    
+};
+
+
+#endif /* Solo_hpp */
